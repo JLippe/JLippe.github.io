b2585d9e808b596a5ad1ae2cfefd87274800630a
JLippe <jesse.lippe@gmail.com>
2016-07-18 19:11:00 +0900

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
 
- $("#gallery").instastream({
+$("#gallery").instastream({
         instaToken: '159193.2bf8cd3.6ab2ae9196f04953a6e3bd90d2ce54fe',
         instaUser: 'tokyojesse',
         instaResults: 3,
