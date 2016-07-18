b7b9d6b6dddd6e7bdc96f151ca9e13696619991a
JLippe <jesse.lippe@gmail.com>
2016-07-18 16:10:44 +0900

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function ($) {
+
+ $("#gallery").instastream({
+        instaToken: '159193.1677ed0.14c21f1263ac475b82c5259e48e6893b',
+        instaUser: 'e87a3f6db27b4ab8a1ac1353482dcd71',
+        instaResults: 3,
+        instaMenu: 'yes'
+    });
+    
+});
